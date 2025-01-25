b25fbe526006d27678c9a9dd97dd3b7e01b45bcc
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2025-01-25 22:51:54 +0530

    added stream actions

---
+++ src/actions/stream.action.ts
@@ -0,0 +1,20 @@
+"use server";
+import { currentUser } from "@clerk/nextjs/server";
+
+import { StreamClient } from "@stream-io/node-sdk";
+
+export const streamTokenProvider = async () => {
+  const user = await currentUser();
+
+  if (!user) {
+    throw new Error("User is not authenticated");
+  }
+  const streamClient = new StreamClient(
+    process.env.NEXT_PUBLIC_STREAM_API_KEY!,
+    process.env.STEAM_SECRET_KEY!
+  );
+
+  const token = streamClient.generateUserToken({ user_id: user.id });
+
+  return token;
+};
